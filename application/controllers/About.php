66d50dadb2a3a700472a969c436fc805a9e012da
Si-Yang Wu <siyang7@hotmail.com>
2018-01-24 10:31:48 -0800

    transformed website into a basic code igniter website
    Signed-off-by: Si-Yang Wu <siyang7@hotmail.com>

---
+++ application/controllers/About.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About extends Application
+{
+
+    /**
+     * About Page for this controller.
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /about/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $this->load->view('about');
+    }
+
+}
---
+++ application/controllers/Gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gallery extends Application
+{
+
+    /**
+     * Gallery page for this controller
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /gallery/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $this->load->view('gallery');
+    }
+
+}
